a3ff5027330eca6c2ab008fa3806a6c40fc1e0f1
Harrison Leggio <Harrison@Harrisons-MacBook-Pro.local>
2017-09-06 22:27:13 -0400

    first push

---
+++ monitor.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import matplotlib.pyplot as plt
+from io import BytesIO
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+
+to_address = '9144737958@mms.att.net'
+from_address = 'harrisonleggio@my.uri.edu'
+
+
+def get_prices():
+    url = 'https://api.gdax.com/products/ltc-usd/ticker'
+    counter = 0
+    prices = []
+    ticks = []
+
+    while counter < 30:
+        response = requests.get(url)
+        data = response.json()
+        prices.append(float(data['price']))
+        ticks.append(counter)
+        counter += 1
+        time.sleep(60)
+
+    plt.plot(ticks, prices)
+
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    mail = MIMEMultipart()
+    mail['Subject'] = 'LTC Update'
+    mail['From'] = from_address
+    mail['To'] = to_address
+    mail.attach(MIMEText('Max Price: {}\nMin Price: {}'.format(max(prices), min(prices))))
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(buf.read())
+    encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % 'anything.png')
+    mail.attach(part)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login('harrisonleggio@my.uri.edu', 'googleplex123')
+    s.sendmail(from_address, to_address, mail.as_string())
+    s.quit()
+    print 'Email sent'
+
+
+if __name__ == '__main__':
+    get_prices()
